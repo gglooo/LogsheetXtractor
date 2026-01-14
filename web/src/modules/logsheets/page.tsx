5a47cc93c02e6e94db7267e2371932fa55bb852b
Jan Glos <jan.glos@inqool.cz>
2026-01-14 01:26:22 +0100

    feat: add logsheet add button to logsheet table

--- web/src/modules/logsheets/page.tsx
+++ web/src/modules/logsheets/page.tsx
@@ -1,10 +1,15 @@
 import { Screen } from "@/components/screen";
+import { Button } from "@/components/ui/button";
 import { LogsheetsNavbar } from "@/modules/logsheets/components/navbar";
 import { LogsheetTable } from "@/modules/logsheets/table/logsheet-table";
-import { useParams } from "react-router-dom";
+import { useIntl } from "react-intl";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const LogsheetsPage = () => {
     const { templateId } = useParams<{ templateId: string }>();
+
+    const navigate = useNavigate();
+    const intl = useIntl();
 
     if (!templateId) {
         return <div className="p-4">No template ID provided.</div>;
@@ -15,6 +20,16 @@ export const LogsheetsPage = () => {
             <LogsheetsNavbar />
             <Screen className="bg-background">
                 <div className="flex flex-col flex-1 px-4">
+                    <Button
+                        variant="outline"
+                        className="mb-4 self-start"
+                        onClick={() => navigate("upload")}
+                    >
+                        {intl.formatMessage({
+                            id: "logsheets.add.button",
+                            defaultMessage: "Add logsheets",
+                        })}
+                    </Button>
                     <LogsheetTable templateId={templateId} className="border" />
                 </div>
             </Screen>
